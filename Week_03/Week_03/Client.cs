a414b792f679b8ccc140a08accf55b6921ffadd1
AvianaLilian <43570065+AvianaLilian@users.noreply.github.com>
2019-06-23 11:06:52 +0200

    Week_03a

---
+++ Week_03/Week_03/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Week_03_Client
+{
+    class Client
+    {
+        private Socket socket_Sender;
+        private IPAddress iPAddress;
+        private IPEndPoint endPoint;
+        private readonly string dataToSend = "<TEMP>";
+
+        public Client()
+        {
+            SetConnection("127.0.0.1", 11000);
+            //OpenConnection();
+        }
+
+        public void SetConnection(string ip, int port)
+        {
+            iPAddress = IPAddress.Parse(ip);
+            endPoint = new IPEndPoint(iPAddress, port);
+        }
+        public void OpenConnection()
+        {
+            socket_Sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket_Sender.Connect(endPoint);
+        }
+        public void CloseConnection()
+        {
+            socket_Sender.Shutdown(SocketShutdown.Both);
+            socket_Sender.Close();
+        }
+        private void SendData(string data)
+        {
+            byte[] message = Encoding.ASCII.GetBytes(data);
+            socket_Sender.Send(message);
+        }
+        public string ReciveData()
+        {
+            SendData(dataToSend);
+            var bytes = new byte[1024];
+            int dataRecived = socket_Sender.Receive(bytes);
+            return Encoding.ASCII.GetString(bytes, 0, dataRecived).Split('>')[1] + "°C";
+        }
+    }
+}
---
+++ Week_03/Week_03/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Timers;
+
+namespace Week_03_Client
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+      
+            Client client;
+            int Itest = 0;
+
+
+            public MainPage()
+            {
+                InitializeComponent();
+                client = new Client();
+                SetTimer();
+            }
+
+            private void SetTimer()
+            {
+                SomeText.Text = "Timer Started";
+                Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+                {
+                    try
+                    {
+                        Itest++;
+                        test.Text = $"I: {Itest}";
+                        client.OpenConnection();
+                        Tempature.Text = $"Temp: {client.ReciveData()}";
+                        client.CloseConnection();
+                        return true; // return true to repeat counting, false to stop timer
+                    }
+                    catch (Exception e)
+                    {
+                        SomeText.Text = e.Message;
+                        return false;
+                    }
+                });            
+            }
+        
+    }
+}
